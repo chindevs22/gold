500c0ae14f8b3ae138cc61bc8c903db44e348ed0
Varsha Gopal <vgopal2@ncsu.edu>
2023-04-18 20:34:22 -0400

    some how the ordering worked

--- archive_gold/plugins/chindevs_plugin/user_events.php
+++ archive_gold/plugins/chindevs_plugin/user_events.php
@@ -26,11 +26,34 @@ add_filter( 'stm_lms_custom_routes_config', function( $routes ) {
     } 
 );
 
+// SOME VERSION OF CHANGING THE STM_LMS_SETTINGS for a specific sorting menu worked
+// i have no clue which one
+// defer to the database blob to figure this out????
+// $my_option = array(
+// 	'id'           => 'user_events',
+// 	'label'        => 'Enrolled Events',
+// 	'menu_place'   => 'learning',
+// );
+
+// // Get the current settings
+// $settings = get_option( 'stm_lms_settings', array() );
+// error_log(print_r($settings,true));
+
+// // // Add your custom option to the options array
+// // error_log("trying toa dd m");
+// // error_log(print_r($settings['sorting_the_menu'],true));
+
+// // // error_log("Trying to add my own setting");
+// // // error_log(print_r($settings['options'],true));
+// // // // Update the stm_lms_settings option with the new settings
+// update_option( 'stm_lms_settings', $settings );
+
+
 add_filter(
 	'stm_lms_menu_items',
 	function ( $menus ) {
 		$menus[] = array(
-			'order'        => 133,
+			'order'        => 105,
 			'id'           => 'user_events',
 			'slug'         => 'user-events',
 			'lms_template' => 'stm-lms-user-events',
