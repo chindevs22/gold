bd24d362b2b36193c004537eee10537f6acd1faf
Varsha Gopal <vgopal2@ncsu.edu>
2023-09-03 19:48:34 -0400

    remove partial points for multiple choice

--- archive_gold/plugins/chindevs_plugin/cd_functions/import_user_progress.php
+++ archive_gold/plugins/chindevs_plugin/cd_functions/import_user_progress.php
@@ -97,7 +97,7 @@ function progress_users_answers_from_csv($answerData) {
     }
 
     $answerString = implode(",", $chosenAnswers); // comma seperated string of answers
-    $isCorrect = ($answerData['question_marks'] != 0) ? "1" : "0"; // Changed for Partial Points
+    $isCorrect = ($answerData['marks_obtained'] != 0) ? "1" : "0"; // Changed for Partial Points
 //    $isCorrect = ($answerData['question_marks'] == $answerData['marks_obtained']) ? "1" : "0";
 
     $attempts = $answerData['running_total'];
